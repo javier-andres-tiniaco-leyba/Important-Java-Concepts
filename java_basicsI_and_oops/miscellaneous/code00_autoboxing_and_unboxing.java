804a3a3278f3e23c620c86a159f6d6c5d82be512
Tiniaco Leyba <45241190+javier-andres-tiniaco-leyba@users.noreply.github.com>
2019-12-30 06:57:05 +0000

    Added autoboxing and unboxing example.

---
+++ java_basicsI_and_oops/miscellaneous/code00_autoboxing_and_unboxing.java
@@ -0,0 +1,23 @@
+// https://docs.oracle.com/javase/tutorial/java/data/QandE/characters-questions.html
+
+class Unboxing {
+
+    public static void main(String[] args) {
+        Integer i = new Integer(-8);
+
+        // 1. Unboxing through method invocation
+        int absVal = absoluteValue(i);
+        System.out.println("absolute value of " + i + " = " + absVal);
+
+        java.util.List<Double> ld = new java.util.ArrayList<>();
+        ld.add(3.1416);    // Π is autoboxed through method invocation.
+
+        // 2. Unboxing through assignment
+        double pi = ld.get(0);
+        System.out.println("pi = " + pi);
+    }
+
+    public static int absoluteValue(int i) {
+        return (i < 0) ? -i : i;
+    }
+}
