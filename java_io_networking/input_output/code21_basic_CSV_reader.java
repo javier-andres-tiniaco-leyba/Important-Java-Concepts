44a8f45f51ff5875479e78514b5b0cd4a35ae100
Tiniaco Leyba <45241190+javier-andres-tiniaco-leyba@users.noreply.github.com>
2020-01-02 07:04:46 +0100

    Added CSV reader example to I/O.

---
+++ java_io_networking/input_output/code21_basic_CSV_reader.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+class CSVReader {
+
+	static final boolean headers = false;
+    static final String  csvFile = "country.csv";
+    static final String  csvSplitBy = ",";
+
+	public static void main(String[] args) {
+		try(BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
+
+			if(headers) {
+				br.readLine();
+            }
+            
+            String  line = "";
+			while((line = br.readLine()) != null) {
+				String[] country = line.split(csvSplitBy);
+				System.out.format(
+                    "Country{code=%s, name=%s}%n",country[4],country[5]);
+			}
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
