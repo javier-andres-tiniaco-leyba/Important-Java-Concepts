d36f0cb75091e0e6d5a560bab139db1c15475bf2
Tiniaco Leyba <45241190+javier-andres-tiniaco-leyba@users.noreply.github.com>
2020-01-11 13:39:41 +0100

    Refactored BigFactorial example.

--- java_basicsII_and_collections/math/code01_BigInteger_factorial.java
+++ java_basicsII_and_collections/math/code01_BigInteger_factorial.java
@@ -11,16 +11,14 @@ class BigFactorial {
 		long resLong = 1;
 		while(i<n) { //overflow
 			System.out.format("factorial(%d)=%d%n",i,resLong);
-            i++;
-            resLong *= i;
+            resLong *= ++i;
         }
 
 		BigInteger res = BigInteger.valueOf(1);
         i = 1;
 		while(i<n) {
 			System.out.format("factorial(%d)=%s%n",i,res);
-            i++;
-			res = res.multiply(BigInteger.valueOf(i));
+			res = res.multiply(BigInteger.valueOf(++i));
 		}
 	}
 
