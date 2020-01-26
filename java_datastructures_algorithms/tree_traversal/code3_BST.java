7dd458fb63099bac622ee7530937cf048c55dcea
Tiniaco Leyba <45241190+javier-andres-tiniaco-leyba@users.noreply.github.com>
2020-01-26 10:43:27 +0100

    Added getHeight iterative method for BST.

--- java_datastructures_algorithms/tree_traversal/code3_BST.java
+++ java_datastructures_algorithms/tree_traversal/code3_BST.java
@@ -1,3 +1,7 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Deque;
+import java.util.LinkedList;
 import java.util.Scanner;
 
 class Node {
@@ -9,12 +13,36 @@ class Node {
     }
 }
 
-class Solution {
+class BST {
+
+    public static int getHeightIterarive(Node root) {
+        if(root == null) return 0;
+        int hmax = 1;
+        int htmp = 1;
+        Deque<Integer> stackH = new LinkedList<>(); stackH.addFirst(0);
+        Deque<Node> stack = new LinkedList<>(); stack.addFirst(null);
+        for(Node current = root; current != null;) {
+            System.out.println(current.data);
+            if(current.right != null) {
+                stackH.addFirst(htmp + 1);
+                stack.addFirst(current.right);
+            }
+            if(current.left != null) {
+                htmp += htmp;
+                current = current.left;
+            } else {
+                current = stack.removeFirst();
+                htmp = stackH.removeFirst();
+                hmax = hmax > htmp ? hmax: htmp;
+            }
+        }
+        return hmax;
+    }
 
 	public static int getHeight(Node root) {
-        int lh = root.left != null ? (1 + getHeight(root.left)) : 0;
-        int rh = root.right != null ? (1 + getHeight(root.right)) : 0;
-        return lh > rh ? lh : rh;
+        int lh = root.left != null ? getHeight(root.left) : 0;
+        int rh = root.right != null ? getHeight(root.right) : 0;
+        return 1 + (lh > rh ? lh : rh);
     }
 
     public static Node insert(Node root, int data) {
@@ -36,24 +64,22 @@ class Solution {
     }
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt();
+        String fileName = "BST.txt";
         Node root = null;
-        while(T-- > 0) {
-            int data = sc.nextInt();
-            root = insert(root,data);
+        try (Scanner sc = new Scanner(new File(fileName))) {
+            int data = 0;
+            while(sc.hasNextInt()) {
+                data = sc.nextInt();
+                //System.out.println(data);
+                root = insert(root,data);
+            }
+        } catch (IOException e) {
+            System.err.format("Could not open file %s%n",fileName);
         }
+
         int height=getHeight(root);
-        System.out.format("%nBST depth: %d%n", height);
+        System.out.format("BST depth(recursive): %d%n", height);
+        height = getHeightIterarive(root);
+        System.out.format("BST depth(Iterative): %d%n", height);
     }
 }
-
-// example input: number of elements + elements:
-// 7
-// 3
-// 5
-// 2
-// 1
-// 4
-// 6
-// 7
